fdd4810f245d45aeb5fd883355ec4b26bcc6f765
Wouter de Wild <wouter@codewild.nl>
2017-08-04 16:47:17 +0200

    Fixed mongodb

--- src/RestBundle/Controller/IndexController.php
+++ src/RestBundle/Controller/IndexController.php
@@ -2,10 +2,18 @@
 
 namespace RestBundle\Controller;
 
-
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 final class IndexController extends Controller
 {
+    public function showAction(Request $request)
+    {
 
+    }
+
+    public function collectionAction(Request $request)
+    {
+
+    }
 }
