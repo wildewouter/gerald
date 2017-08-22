41199fe403e92c97c3e7c5e25b81401e8f32fc23
Wouter de Wild <wouter@codewild.nl>
2017-08-22 09:59:08 +0200

    📖 Added file download docs

--- src/Document/FileBundle/Controller/FileController.php
+++ src/Document/FileBundle/Controller/FileController.php
@@ -2,6 +2,7 @@
 
 namespace Document\FileBundle\Controller;
 
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -13,6 +14,19 @@ final class FileController extends Controller
     /**
      * @Route("/file/{fileName}", name="document.file.download")
      * @Method("GET")
+     *
+     * @ApiDoc(
+     *  resource=true,
+     *  description="Create a new document",
+     *  requirements={
+     *      {
+     *          "name"="fileName",
+     *          "dataType"="string",
+     *          "requirement"="\w+",
+     *          "description"="Name of the requested file"
+     *      }
+     *  },
+     * )
      *
      * @param string $fileName
      * @return StreamedResponse
