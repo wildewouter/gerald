5ccdfbedf8b08e267d566f87dfb3366644f8a7a1
Wouter de Wild <wouter@codewild.nl>
2017-08-30 11:45:50 +0200

    🛠 added .env and upgraded tooling

--- web/app.php
+++ web/app.php
@@ -1,20 +1,21 @@
 <?php
 
+use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
 
 require __DIR__.'/../vendor/autoload.php';
-if (PHP_VERSION_ID < 70000) {
-    include_once __DIR__.'/../var/bootstrap.php.cache';
+$dotenv = new Dotenv();
+$dotenv->load(__DIR__ . '/../.env');
+
+$isDebug = getenv('DEBUG');
+
+if ($isDebug) {
+    Debug::enable();
 }
 
-$kernel = new AppKernel('prod', false);
-if (PHP_VERSION_ID < 70000) {
-    $kernel->loadClassCache();
-}
-//$kernel = new AppCache($kernel);
+$kernel = new AppKernel(getenv('ENV'), $isDebug);
 
-// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
-//Request::enableHttpMethodParameterOverride();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
